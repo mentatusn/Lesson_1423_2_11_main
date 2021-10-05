bbcd8d371f938c53482c44358c48acd900adda96
Andej Nesterenko <mentatus.n@gmail.com>
2021-10-05 21:13:58 +0300

    Initial commit

---
+++ app/src/main/java/ru/geekbrains/lesson_1423_2_11_main/MainActivity.kt
@@ -0,0 +1,13 @@
+package ru.geekbrains.lesson_1423_2_11_main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        findViewById<TextView>(R.id.textView).text = BuildConfig.BUILD_TYPE
+    }
+}
