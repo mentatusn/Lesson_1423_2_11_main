b1c0a7b1e8d03c336f7231f320440625e373e131
Andej Nesterenko <mentatus.n@gmail.com>
2021-10-05 21:38:07 +0300

    Идет урок 12, забили гвоздь пасатижами( то есть использовали функционал buildTypes, как если бы это были flavors)

--- app/src/main/java/ru/geekbrains/lesson_1423_2_11_main/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/lesson_1423_2_11_main/MainActivity.kt
@@ -3,11 +3,17 @@ package ru.geekbrains.lesson_1423_2_11_main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import java.lang.IllegalStateException
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        findViewById<TextView>(R.id.textView).text = BuildConfig.BUILD_TYPE
+
+
+        when(BuildConfig.field1){
+            3->        findViewById<TextView>(R.id.textView).text = "Здравствуйте company1"
+            4->        findViewById<TextView>(R.id.textView).text =  "Здравствуйте company2"
+        }
     }
 }
